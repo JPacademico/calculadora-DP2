f80ef7563931fc473ff28457c8fdff7c23c0d98a
JPacademico <ximbas@academico.ufs.br>
2023-12-19 17:21:31 -0300

    commit epico

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const valorEquipamento = document.getElementById('equipamento')
+const valorSucata = document.getElementById('sucata')
+const valorUtil = document.getElementById('vida-util')
+const valorUso = document.getElementById('tempo-uso')
+
+document.getElementById('calcula').addEventListener('click', function())
