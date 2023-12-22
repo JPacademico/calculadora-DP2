bdc261d9e036f381bbd15cd5d6b997aa5cfe0ea5
JPacademico <ximbas@academico.ufs.br>
2023-12-22 01:48:27 -0300

    commit lendario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-const valorEquipamento = document.getElementById('equipamento')
-const valorSucata = document.getElementById('sucata')
-const valorUtil = document.getElementById('vida-util')
-const valorUso = document.getElementById('tempo-uso')
 
-document.getElementById('calcula').addEventListener('click', function())
+
+document.getElementById('calcula').addEventListener('click', function(){
+    let valorEquipamento = document.getElementById('equipamento').value
+    let valorSucata = document.getElementById('sucata').value
+    let valorUtil = document.getElementById('vida-util').value
+    let valorUso = document.getElementById('tempo-uso').value
+
+    let DP = (valorEquipamento - valorSucata) / valorUtil * valorUso
+
+    if(valorEquipamento == "" || valorSucata == "" || valorUtil == "" || valorUso == ""){
+        document.getElementById('result').innerHTML = "Inválido";
+    }else{
+        document.getElementById('result').innerHTML = "R$" + DP.toFixed(2);
+        
+    }
+    
+
+});
