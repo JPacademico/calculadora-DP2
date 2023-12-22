f7fe20f53c4aa642b8eb2b46c089fd9ffd6288df
JPacademico <ximbas@academico.ufs.br>
2023-12-22 16:53:22 -0300

    commit insano

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,18 @@ document.getElementById('calcula').addEventListener('click', function(){
     let valorUtil = document.getElementById('vida-util').value
     let valorUso = document.getElementById('tempo-uso').value
 
-    let DP = (valorEquipamento - valorSucata) / valorUtil * valorUso
+    let valorPorcentagem = valorEquipamento * 0.1;
+    let DP = (valorEquipamento - valorSucata) / valorUtil;
+    let DPM = (valorEquipamento * valorPorcentagem) / 12;
+
+    console.log(DP)
 
     if(valorEquipamento == "" || valorSucata == "" || valorUtil == "" || valorUso == ""){
         document.getElementById('result').innerHTML = "Inválido";
+        document.getElementById('result-2').innerHTML = "Inválido";
     }else{
-        document.getElementById('result').innerHTML = "R$" + DP.toFixed(2);
+        document.getElementById('result').innerHTML = "Depreciação anual: R$" + DP.toFixed(2);
+        document.getElementById('result-2').innerHTML = "Mensal Fiscal: R$" + DPM.toFixed(2);
         
     }
     
